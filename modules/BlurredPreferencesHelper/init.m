7972fa7ec259e911f452e596974c88e520dd2f96
Jinwoo Kim <kidjinwoo@me.com>
2022-09-28 22:10:26 +0900

    no message

--- modules/BlurredPreferencesHelper/init.m
+++ modules/BlurredPreferencesHelper/init.m
@@ -1,18 +1,21 @@
 #import <UIKit/UIKit.h>
 #import <libhooker/libhooker.h>
-#import <Preferences/PSSplitViewController.h>
-#import <PreferencesUI/PSUIPrefsListController.h>
 
 static void (*original_UIViewController_viewDidLoad)(UIViewController *self, SEL selector);
 static void custom_UIViewController_viewDidLoad(UIViewController * self, SEL selector) {
-   original_UIViewController_viewDidLoad(self, selector);
-   [self.view setBackgroundColor:UIColor.clearColor];
+    original_UIViewController_viewDidLoad(self, selector);
+    if ([self isKindOfClass:NSClassFromString(@"PSSplitViewController")]) {
+        // if I set clearColor or alpha value as 0.0f, touch event doesn't work correctly - why?
+        [self.view setBackgroundColor:[self.view.backgroundColor colorWithAlphaComponent:0.001f]];
+    } else {
+        [self.view setBackgroundColor:UIColor.clearColor];
+    }
 }
 
 static void (*original_UIViewController_viewWillAppear)(UIViewController *self, SEL selector, BOOL animated);
 static void custom_UIViewController_viewWillAppear(UIViewController * self, SEL selector, BOOL animated) {
-   original_UIViewController_viewWillAppear(self, selector, animated);
-   [self.transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+    original_UIViewController_viewWillAppear(self, selector, animated);
+    [self.transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         [self.view setAlpha:1.0f];
     } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         
@@ -21,9 +24,9 @@ static void custom_UIViewController_viewWillAppear(UIViewController * self, SEL 
 
 static void (*original_UIViewController_viewWillDisappear)(UIViewController *self, SEL selector, BOOL animated);
 static void custom_UIViewController_viewWillDisappear(UIViewController * self, SEL selector, BOOL animated) {
-   original_UIViewController_viewWillDisappear(self, selector, animated);
+    original_UIViewController_viewWillDisappear(self, selector, animated);
    
-   [self.transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
+    [self.transitionCoordinator animateAlongsideTransition:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         [self.view setAlpha:0.0f];
     } completion:^(id<UIViewControllerTransitionCoordinatorContext>  _Nonnull context) {
         
@@ -32,6 +35,7 @@ static void custom_UIViewController_viewWillDisappear(UIViewController * self, S
 
 static void (*original_UITableView_setBackgroundColor)(UITableView *self, SEL selector, UIColor *backgroundColor);
 static void custom_UITableView_setBackgroundColor(UITableView *self, SEL selector, UIColor *backgroundColor) {
+    NSLog(@"%@, %@", self, backgroundColor);
     original_UITableView_setBackgroundColor(self, selector, UIColor.clearColor);
 }
 
@@ -55,7 +59,7 @@ __attribute__((constructor)) static void init() {
     LBHookMessage(UIViewController.class, @selector(viewWillAppear:), &custom_UIViewController_viewWillAppear, &original_UIViewController_viewWillAppear);
     LBHookMessage(UIViewController.class, @selector(viewWillDisappear:), &custom_UIViewController_viewWillDisappear, &original_UIViewController_viewWillDisappear);
     LBHookMessage(UITableView.class, @selector(setBackgroundColor:), &custom_UITableView_setBackgroundColor, &original_UITableView_setBackgroundColor);
-    // LBHookMessage(UITableView.class, @selector(backgroundColor), &custom_UITableView_backgroundColor, &original_UITableView_backgroundColor);
+    LBHookMessage(UITableView.class, @selector(backgroundColor), &custom_UITableView_backgroundColor, &original_UITableView_backgroundColor);
     LBHookMessage(UITableViewCell.class, @selector(setBackgroundColor:), &custom_UITableViewCell_setBackgroundColor, &original_UITableViewCell_setBackgroundColor);
     // LBHookMessage(UITableViewCell.class, @selector(backgroundColor), &custom_UITableViewCell_backgroundColor, &original_UITableViewCell_backgroundColor);
 }

c878dc053a43ee0cd508d02266855bb009bd23d9
Jinwoo Kim <kidjinwoo@me.com>
2022-12-05 16:03:11 +0900

    no message

--- modules/BlurredPreferencesSpringBoardHelper/init.m
+++ modules/BlurredPreferencesSpringBoardHelper/init.m
@@ -55,12 +55,12 @@ static NSString * const kPreferencesBundleIdentifier = @"com.apple.Preferences";
 long (*original_SBDeviceApplicationSceneHandle_backgroundStyle)(SBDeviceApplicationSceneHandle *self, SEL selector);
 long custom_SBDeviceApplicationSceneHandle_backgroundStyle(SBDeviceApplicationSceneHandle *self, SEL selector) {
     if ([self.application.bundleIdentifier isEqualToString:kPreferencesBundleIdentifier]) {
-        id settings = self.sceneIfExists.settings;
-        if (![settings isKindOfClass:NSClassFromString(@"UIApplicationSceneSettings")]) {
-            return original_SBDeviceApplicationSceneHandle_backgroundStyle(self, selector); 
+        UIApplicationSceneSettings *settings = (UIApplicationSceneSettings *)self.sceneIfExists.settings;
+        if ((settings == nil) || (![settings isKindOfClass:NSClassFromString(@"UIApplicationSceneSettings")])) {
+            return original_SBDeviceApplicationSceneHandle_backgroundStyle(self, selector);
         }
 
-        long userInterfaceStyle = ((UIApplicationSceneSettings *)settings).userInterfaceStyle;
+        long userInterfaceStyle = settings.userInterfaceStyle;
 
         // UIBackgroundStyleDarkBlur, UIBackgroundStyleUltraLightBlur, UIBackgroundStyleDefault
         if (userInterfaceStyle == 1) {
@@ -68,36 +68,36 @@ long custom_SBDeviceApplicationSceneHandle_backgroundStyle(SBDeviceApplicationSc
         } else if (userInterfaceStyle == 2) {
             return 4; // UIBackgroundStyleDarkBlur
         } else {
-            return original_SBDeviceApplicationSceneHandle_backgroundStyle(self, selector); 
+            return original_SBDeviceApplicationSceneHandle_backgroundStyle(self, selector);
         }
     } else {
         return original_SBDeviceApplicationSceneHandle_backgroundStyle(self, selector);
     } 
 }
 
-BOOL (*original_SBDeviceApplicationSceneHandle_isTranslucent)(SBDeviceApplicationSceneHandle *self, SEL selector);
-BOOL custom_SBDeviceApplicationSceneHandle_isTranslucent(SBDeviceApplicationSceneHandle *self, SEL selector) {
-    if ([self.application.bundleIdentifier isEqualToString:kPreferencesBundleIdentifier]) {
-        return YES;
-    } else {
-        return original_SBDeviceApplicationSceneHandle_isTranslucent(self, selector);
-    } 
-}
+// BOOL (*original_SBDeviceApplicationSceneHandle_isTranslucent)(SBDeviceApplicationSceneHandle *self, SEL selector);
+// BOOL custom_SBDeviceApplicationSceneHandle_isTranslucent(SBDeviceApplicationSceneHandle *self, SEL selector) {
+//     if ([self.application.bundleIdentifier isEqualToString:kPreferencesBundleIdentifier]) {
+//         return YES;
+//     } else {
+//         return original_SBDeviceApplicationSceneHandle_isTranslucent(self, selector);
+//     } 
+// }
 
 void (*original_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings)(SBDeviceApplicationSceneView *self, SEL selector, id diff, id previous);
 void custom_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings(SBDeviceApplicationSceneView *self, SEL selector, id diff, id previous) {
     original_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings(self, selector, diff, previous);
     if ([self.application.bundleIdentifier isEqualToString:kPreferencesBundleIdentifier]) {
-        id sceneHandle = self.sceneHandle;
+        SBDeviceApplicationSceneHandle *sceneHandle = (SBDeviceApplicationSceneHandle *)self.sceneHandle;
         if (![sceneHandle isKindOfClass:NSClassFromString(@"SBDeviceApplicationSceneHandle")]) return;
-        self.backgroundView.wallpaperStyle = ((SBDeviceApplicationSceneHandle *)sceneHandle).wallpaperStyle;
+        self.backgroundView.wallpaperStyle = sceneHandle.wallpaperStyle;
     }
 }
 
 __attribute__((constructor)) static void init() {
     @autoreleasepool {
         MSHookMessageEx(NSClassFromString(@"SBDeviceApplicationSceneHandle"), @selector(backgroundStyle), (IMP)&custom_SBDeviceApplicationSceneHandle_backgroundStyle, (IMP *)&original_SBDeviceApplicationSceneHandle_backgroundStyle);
-        MSHookMessageEx(NSClassFromString(@"SBDeviceApplicationSceneHandle"), @selector(isTranslucent), (IMP)&custom_SBDeviceApplicationSceneHandle_isTranslucent, (IMP *)&original_SBDeviceApplicationSceneHandle_isTranslucent);
-        MSHookMessageEx(NSClassFromString(@"SBDeviceApplicationSceneView"), @selector(_sceneHandleDidUpdateSettingsWithDiff:previousSettings:), (IMP)custom_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings, (IMP *)&original_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings);
+        // MSHookMessageEx(NSClassFromString(@"SBDeviceApplicationSceneHandle"), @selector(isTranslucent), (IMP)&custom_SBDeviceApplicationSceneHandle_isTranslucent, (IMP *)&original_SBDeviceApplicationSceneHandle_isTranslucent);
+        MSHookMessageEx(NSClassFromString(@"SBDeviceApplicationSceneView"), @selector(_sceneHandleDidUpdateSettingsWithDiff:previousSettings:), (IMP)&custom_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings, (IMP *)&original_SBDeviceApplicationSceneView_sceneHandleDidUpdateSettingsWithDiff_previousSettings);
     }
 }
